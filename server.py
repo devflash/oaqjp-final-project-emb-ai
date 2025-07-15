7f4825d2fdf6ea3bf7eeef92404cde82f6a409fa
Mrugesh Wadagbalkar <mwadagbalkar@eisgroup.com>
2025-07-16 00:21:21 +0100

    run code analysis

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+'''Emotion detector server'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,14 +6,19 @@ app = Flask("Emotion detector")
 
 @app.route("/emotionDetector")
 def detect_emotion():
-    textToAnalyze = request.args.get("textToAnalyze")
+    '''Function that calls emotion_detector'''
+    text_to_analyse = request.args.get("textToAnalyze")
 
-    result = emotion_detector(textToAnalyze)
+    result = emotion_detector(text_to_analyse)
     dominant_emotion = result["dominant_emotion"]
+    if dominant_emotion == 'None':
+        return 'Invalid text! Please try again!.'
+
     return f"For the given statement, the system response is '{dominant_emotion}': {result}"
 
 @app.route("/")
 def render_index_page():
+    '''Function that renders HTML'''
     return render_template('index.html')
 
-app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5001)
